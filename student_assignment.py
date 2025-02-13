afcc6abde27d048f427748b053a3624d5224a77a
quinton_cheng <quinton_cheng@asus.com>
2025-02-13 15:15:50 +0800

    testing

--- student_assignment.py
+++ student_assignment.py
@@ -1,3 +1,4 @@
+aa
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
